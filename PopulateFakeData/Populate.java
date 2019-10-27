5e8ce0b6077f6ad28c21ac1054c4ef1e7f255cee
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-10-27 13:07:09 -0500

    Added weights to majors/double majors/school years

--- PopulateFakeData/Populate.java
+++ PopulateFakeData/Populate.java
@@ -7,18 +7,28 @@ public class Populate
     private static ArrayList<Member> members;
     private static ArrayList<String> fakeNames;
     private static ArrayList<String> majors;
+    private static ArrayList<Float> majorsWeights;
     private static ArrayList<String> doubleMajors;
+    private static ArrayList<Float> doubleMajorsWeights;
     private static ArrayList<String> schoolYears;
+    private static ArrayList<Float> schoolYearsWeights;
+        
+    private static Random r;
 
     public static void main(String[] args) throws IOException
     {
         DecimalFormat df = new DecimalFormat("0000");
+
+        r = new Random();
         
         members = new ArrayList<Member>();
         fakeNames = new ArrayList<String>();
         majors = new ArrayList<String>();
+        majorsWeights = new ArrayList<Float>();
         doubleMajors = new ArrayList<String>();
+        doubleMajorsWeights = new ArrayList<Float>();
         schoolYears = new ArrayList<String>();
+        schoolYearsWeights = new ArrayList<Float>();
 
         Scanner inFakeNames = new Scanner(new File("random_names.txt"));
         
@@ -31,9 +41,11 @@ public class Populate
 
         Scanner inMajors = new Scanner(new File("majors.txt"));
 
+        float weight = 0.0f;
         while (inMajors.hasNext())
         {
-            majors.add(inMajors.nextLine());
+            majorsWeights.add(Float.parseFloat(inMajors.nextLine().trim()));
+            majors.add(inMajors.nextLine().trim());
         }
 
         inMajors.close();
@@ -42,7 +54,8 @@ public class Populate
 
         while (inDoubleMajors.hasNext())
         {
-            doubleMajors.add(inDoubleMajors.nextLine());
+            doubleMajorsWeights.add(Float.parseFloat(inDoubleMajors.nextLine().trim()));
+            doubleMajors.add(inDoubleMajors.nextLine().trim());
         }
 
         inDoubleMajors.close();
@@ -51,7 +64,8 @@ public class Populate
 
         while (inSchoolYears.hasNext())
         {
-            schoolYears.add(inSchoolYears.nextLine());
+            schoolYearsWeights.add(Float.parseFloat(inSchoolYears.nextLine().trim()));
+            schoolYears.add(inSchoolYears.nextLine().trim());
         }
 
         inSchoolYears.close();
@@ -60,14 +74,13 @@ public class Populate
         String major = "";
         String doubleMajor = "";
         String year = "";
-        
-        Random r = new Random();
 
         for (String s : fakeNames)
         {
             parts = s.split(" ");
 
-            major = majors.get(r.nextInt(majors.size()));
+            // major = majors.get(r.nextInt(majors.size()));
+            major = getRandomMajor();
             if (major.equals("Undeclared"))
             {
                 doubleMajor = "Undeclared";
@@ -75,17 +88,65 @@ public class Populate
             }
             else
             {
-                doubleMajor = doubleMajors.get(r.nextInt(doubleMajors.size()));
-                year = schoolYears.get(r.nextInt(schoolYears.size()));
+                // doubleMajor = doubleMajors.get(r.nextInt(doubleMajors.size()));
+                // year = schoolYears.get(r.nextInt(schoolYears.size()));
+                doubleMajor = getRandomDoubleMajor();
+                year = getRandomSchoolYear();
             }
             
             members.add(new Member(parts[0], parts[1], major, doubleMajor, year));
         }
 
-        PopulateMembers();
+        populateMembers();
     }
 
-    private static void PopulateMembers()
+    private static String getRandomMajor()
+    {
+        float weight = r.nextFloat();
+        System.out.println(weight);
+        System.out.println(majorsWeights.size());
+        for(int i = majorsWeights.size() - 1; i >= 0; i--)
+        {
+            System.out.println(weight + "\t" + majorsWeights.get(i));
+            if (weight <= majorsWeights.get(i))
+            {
+                return majors.get(i);
+            }
+        }
+
+        System.out.println();
+        return majors.get(0);
+    }
+
+    private static String getRandomDoubleMajor()
+    {
+        float weight = r.nextFloat();
+        for(int i = doubleMajorsWeights.size() - 1; i >= 0; i--)
+        {
+            if (weight <= doubleMajorsWeights.get(i))
+            {
+                return doubleMajors.get(i);
+            }
+        }
+
+        return doubleMajors.get(0);
+    }
+
+    private static String getRandomSchoolYear()
+    {
+        float weight = r.nextFloat();
+        for(int i = schoolYearsWeights.size() - 1; i >= 0; i--)
+        {
+            if (weight <= schoolYearsWeights.get(i))
+            {
+                return schoolYears.get(i);
+            }
+        }
+
+        return schoolYears.get(0);
+    }
+
+    private static void populateMembers()
     {
 
         StringBuilder sb = new StringBuilder();
